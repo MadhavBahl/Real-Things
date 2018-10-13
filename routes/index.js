724b62ce2e2990b770b63856ca0b1bf5c8ed086f
Madhav Bahl <madhavbahl10@gmail.com>
2018-10-14 00:33:30 +0530

    Test

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-	res.render('index.hbs');
+router.get('/home', function (req, res, next) {
+	res.render ('index.hbs');
+});
+
+router.get ('/', function (req, res, next) {
+	res.render ('testNot.hbs');
 });
 
 module.exports = router;
