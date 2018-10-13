26a592a2cea2f7f33ddca5a85f3df0a2faa04b13
Madhav Bahl <madhavbahl10@gmail.com>
2018-10-14 02:02:15 +0530

    Add test SMS

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 require('dotenv').config();
 var request = require('request');
+var unirest = require('unirest');
 
 /* GET home page. */
 router.get('/home', function (req, res, next) {
@@ -13,15 +14,21 @@ router.get ('/', function (req, res, next) {
 });
 
 router.get('/sendMessage', (req, res) => {
-	request(`http://api.msg91.com/api/sendhttp.php?country=91&sender=MSGIND&route=4&mobiles=918141491833,918800467915&authkey=${process.env.SMS_API}&message=Hey BHAI sdmaslkmassCHALJAA Yo Dude!`, function (error, response, body) {
+	// request(`http://api.msg91.com/api/sendhttp.php?country=91&sender=MSGIND&route=4&mobiles=918141491833,918800467915&authkey=${process.env.SMS_API}&message=Hey BHAI sdmaslkmassCHALJAA Yo Dude!`, function (error, response, body) {
 		
-		if (error) {
-			res.status(404).send(error);
-		}
-		console.log('body:', body); // Print the HTML for the Google homepage.
-		console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-		res.render('dashboard.hbs');
-	});
+	// 	if (error) {
+	// 		res.status(404).send(error);
+	// 	}
+	// 	console.log('body:', body); // Print the HTML for the Google homepage.
+	// 	console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+	// 	res.render('dashboard.hbs');
+	// });
+
+	unirest.post(`http://api.msg91.com/api/sendhttp.php?country=91&sender=MSGIND&route=4&mobiles=918800467915&authkey=${process.env.SMS_API}&message=BHAI sdmaslkmassCHALJAA Yo Dude!`)
+		.headers({'Accept': 'application/json'})
+		.end(function (response) {
+			res.render('dashboard.hbs');
+		});
 });
 
 module.exports = router;
