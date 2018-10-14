571538c4c2f74f6981573144464194fe721dd192
Madhav Bahl <madhavbahl10@gmail.com>
2018-10-14 15:21:07 +0530

    Complpete dashboard

--- public/mainData.js
+++ public/mainData.js
@@ -14,17 +14,65 @@ ref.on("value", function(snapshot) {
         violence.innerHTML = snapshot.val().test.violence.violent_scenario;
         var drowsy = snapshot.val().test.drowsiness.status;
         var violent= snapshot.val().test.violence.violent_scenario;
+
+        // var mobNums = snapshot.val().test.closeContacts;
+
+        var mobString = '';
+        var i = 0;
+        for (var mobNum in snapshot.val().test.closeContacts) {
+            if (i == 0) {
+                mobString = snapshot.val().test.closeContacts[mobNum];
+            } else {
+                mobString += ','+snapshot.val().test.closeContacts[mobNum];
+            }
+            i++;
+        }
+
+        // alert (mobString);
         if (drowsy === "YES") {
-            $.get("/drowsy", function(data, status){
-                console.log('Inside the /drowsy');
+            // $.get("/drowsy", function(data, status){
+            //     console.log('Inside the /drowsy');
+            // });
+
+            $.post("/drowsy", {mobString: mobString}, function(result){
+                // $("span").html(result);
+                console.log('inside Violent')
             });
         }
 
         if (violent === "YES") {
-            $.get("/violence", function(data, status){
-                console.log('Inside the /violent');
+            // $.get("/violence", function(data, status){
+            //     console.log('Inside the /violent');
+            // });
+
+            $.post("/violence", {mobString: mobString}, function(result){
+                // $("span").html(result);
+                console.log('inside Violent')
             });
         }
     }, function (error) {
     console.log("Error: " + error.code);
 });
+
+function addPhone () {
+    ref.on("value", function(snapshot) {
+        console.log('button clicked:', snapshot.val());
+            // var numContacts = snapshot.val().test.closeContacts.length;
+            // console.log(snapshot.val().test.closeContacts);
+            
+            var uname = $('#name').val();
+            var mobile = $('#mobile').val();
+
+            
+            var toBeAdded = {};
+            toBeAdded[uname] = mobile;
+            var db = firebase.database();
+            var ref = db.ref("test");
+            var contacts = ref.child("closeContacts");
+            contacts.update(toBeAdded);
+            alert ('data added!');
+
+        }, function (error) {
+        console.log("Error: " + error.code);
+    });
+}
--- routes/index.js
+++ routes/index.js
@@ -13,8 +13,10 @@ router.get('/', function (req, res, next) {
 // 	res.render ('testNot.hbs');
 // });
 
-router.get('/violence', (req, res) => {
-	unirest.post(`http://api.msg91.com/api/sendhttp.php?country=91&sender=MSGIND&route=4&mobiles=918800467915&authkey=242704A8yXSd0INO5bc24274&message=DRIVER IS VIOLENT! CALL POLICE NOW!`)
+router.post('/violence', (req, res) => {
+	var mobString = req.body.mobString;
+	console.log(mobString);
+	unirest.post(`http://api.msg91.com/api/sendhttp.php?country=91&sender=MSGIND&route=4&mobiles=${mobString}&authkey=242704A8yXSd0INO5bc24274&message=DRIVER IS VIOLENT! CALL POLICE NOW!`)
 		.headers({'Accept': 'application/json'})
 		.end(function (response) {
 			console.log('response is: ', response.body);
@@ -22,8 +24,9 @@ router.get('/violence', (req, res) => {
 		});
 });
 
-router.get('/drowsy', (req, res) => {
-	unirest.post(`http://api.msg91.com/api/sendhttp.php?country=91&sender=MSGIND&route=4&mobiles=918800467915&authkey=242704A8yXSd0INO5bc24274&message=HEY! The driver is feeling sleepy! TAKE ACTION NOW!!!!`)
+router.post('/drowsy', (req, res) => {
+	var mobString = req.body.mobString;
+	unirest.post(`http://api.msg91.com/api/sendhttp.php?country=91&sender=MSGIND&route=4&mobiles=${mobString}&authkey=242704A8yXSd0INO5bc24274&message=HEY! The driver is feeling sleepy! TAKE ACTION NOW!!!!`)
 		.headers({'Accept': 'application/json'})
 		.end(function (response) {
 			res.render('dashboard.hbs');
