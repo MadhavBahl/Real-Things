b15ca739a1978d906b74ec20eba79f3b0851173f
Hrishi <hrishipatel99@gmail.com>
2018-10-14 15:21:55 +0530

    Updates

--- PyCode/drow.py
+++ PyCode/drow.py
@@ -1,8 +1,3 @@
-# USAGE
-# python detect_drowsiness.py --shape-predictor shape_predictor_68_face_landmarks.dat
-# python detect_drowsiness.py --shape-predictor shape_predictor_68_face_landmarks.dat --alarm alarm.wav
-
-# import the necessary packages
 from scipy.spatial import distance as dist
 from imutils.video import VideoStream
 from imutils import face_utils
@@ -104,30 +99,20 @@ def classify(frame):
 
 	print('\nEvaluation time (1-image): {:.3f}s\n'.format(end-start))
 	template = "{} (score={:0.5f})"
+	cv2.putText(frame, labels[0], (10, 70),
+					cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+	db.child(args["user"]).child("violence").update({"violent_scenario": labels[0].upper()})
 	for i in top_k:
-		#print(template.format(labels[i], results[i]))
+		print(template.format(labels[i], results[i]))
 		print(labels[i])
-		db.child(args["user"]).child("violence").update({"violent_scenario": labels[0].upper()})
-
-
     	
 def eye_aspect_ratio(eye):
-	# compute the euclidean distances between the two sets of
-	# vertical eye landmarks (x, y)-coordinates
 	A = dist.euclidean(eye[1], eye[5])
 	B = dist.euclidean(eye[2], eye[4])
-
-	# compute the euclidean distance between the horizontal
-	# eye landmark (x, y)-coordinates
 	C = dist.euclidean(eye[0], eye[3])
-
-	# compute the eye aspect ratio
 	ear = (A + B) / (2.0 * C)
-
-	# return the eye aspect ratio
 	return ear
  
-# construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-p", "--shape-predictor", default="sp.dat",
 	help="path to facial landmark predictor")
@@ -139,43 +124,28 @@ ap.add_argument("-v", "--violence", type=int ,default=0,
 	help="0 to disable violence detection, 1 to enable")
 args = vars(ap.parse_args())
  
-# define two constants, one for the eye aspect ratio to indicate
-# blink and then a second constant for the number of consecutive
-# frames the eye must be below the threshold for to set off the
-# alarm
-EYE_AR_THRESH = 0.30
+EYE_AR_THRESH = 0.26
 if args["violence"]==1:
 	EYE_AR_CONSEC_FRAMES = 20
 else:
 	EYE_AR_CONSEC_FRAMES = 48
 print(str(EYE_AR_CONSEC_FRAMES))
 
-# initialize the frame counter as well as a boolean used to
-# indicate if the alarm is going off
 COUNTER = 0
-# initialize dlib's face detector (HOG-based) and then create
-# the facial landmark predictor
 print("[INFO] loading facial landmark predictor...")
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor(args["shape_predictor"])
 
-# grab the indexes of the facial landmarks for the left and
-# right eye, respectively
 (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
 (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
 
-# start the video stream thread
 print("[INFO] starting video stream thread...")
 vs = cv2.VideoCapture(args["webcam"])
 time.sleep(1.0)
 
-# loop over frames from the video stream
 timeout = time.time() + 30
 while True:
 	test=0
-	# grab the frame from the threaded video file stream, resize
-	# it, and convert it to grayscale
-	# channels)'
 	global frame
 	ret, frame = vs.read()
 	frame = imutils.resize(frame, width=450)
@@ -186,43 +156,36 @@ while True:
 
 	if args["violence"]==1:
 		if test%2==0:
-			classify(frame)
+			t = Thread(target=classify,
+			args=(frame,))
+			t.deamon = True
+			t.start()
+			#classify(frame)
 	test = test-1
 	''''thread = RepeatEvery(100, classify, frame)
 	thread.start()
 	thread.join(1000)  # allow thread to execute a while...
 	thread.stop()'''
 
-	# loop over the face detections
 	for rect in rects:
-		# determine the facial landmarks for the face region, then
-		# convert the facial landmark (x, y)-coordinates to a NumPy
-		# array
 		shape = predictor(gray, rect)
 		shape = face_utils.shape_to_np(shape)
 
-		# extract the left and right eye coordinates, then use the
-		# coordinates to compute the eye aspect ratio for both eyes
 		leftEye = shape[lStart:lEnd]
 		rightEye = shape[rStart:rEnd]
 		leftEAR = eye_aspect_ratio(leftEye)
 		rightEAR = eye_aspect_ratio(rightEye)
 
-		# average the eye aspect ratio together for both eyes
 		ear = (leftEAR + rightEAR) / 2.0
 
-		# compute the convex hull for the left and right eye, then
-		# visualize each of the eyes
 		leftEyeHull = cv2.convexHull(leftEye)
 		rightEyeHull = cv2.convexHull(rightEye)
 		cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 255), 1)
 		cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 255), 1)
 
-		# check to see if the eye aspect ratio is below the blink
-		# threshold, and if so, increment the blink frame counter
 		returnValue = (yawn.yawnDetector(frame), 'yawn')
 		if returnValue[0]:
-			print("Yawn detected!")
+			#print("Yawn detected!")
             # When everything isdone, release the capture
 			#print(returnValue)
 			cv2.putText(frame, str(yawn.yawnCounter), (10, 50),
